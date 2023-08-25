a22e14da63dc636dd7b71240d37c6aa8fdafa1fa
Innei <i@innei.in>
2023-08-25 19:11:43 +0800

    build: rollup and testing

---
+++ rollup.config.ts
@@ -0,0 +1,94 @@
+import { rmSync } from 'fs'
+import browserslist from 'browserslist'
+import { defineConfig } from 'rollup'
+import dtsExports from 'rollup-plugin-dts'
+import { preserveUseDirective, swc } from 'rollup-plugin-swc3'
+
+import pkgJson from './package.json'
+import { getEntries } from './tools/get-entries.js'
+
+// @ts-expect-error -- rollup-plugin-dts has incorrect types
+const dts = dtsExports.default as typeof dtsExports
+
+const externalModules = Object.keys(pkgJson.dependencies)
+  .concat(Object.keys(pkgJson.peerDependencies))
+  .concat(['react-router-dom', 'next'])
+const external = (id: string) => {
+  return externalModules.some(
+    (name) => id === name || id.startsWith(`${name}/`),
+  )
+}
+
+// Same target as Next.js 13
+const targets = browserslist([
+  'chrome 64',
+  'edge 79',
+  'firefox 67',
+  'opera 51',
+  'safari 12',
+])
+
+export default async function () {
+  rmSync('dist', { recursive: true, force: true })
+
+  const input = await getEntries()
+
+  return defineConfig([
+    {
+      input,
+      output: [
+        {
+          dir: 'dist',
+          format: 'commonjs',
+          entryFileNames: '[name]/index.cjs',
+        },
+        {
+          dir: 'dist',
+          format: 'commonjs',
+          entryFileNames: '[name]/index.js',
+        },
+        {
+          dir: 'dist',
+          format: 'esm',
+          entryFileNames: '[name]/index.mjs',
+        },
+      ],
+      plugins: [
+        swc({
+          isModule: true,
+          jsc: {
+            target: undefined,
+            transform: {
+              react: {
+                runtime: 'automatic',
+              },
+            },
+            minify: {
+              compress: {
+                passes: 2,
+              },
+              mangle: {},
+              module: true,
+            },
+          },
+          minify: true,
+          env: {
+            targets,
+          },
+        }),
+        preserveUseDirective(),
+      ],
+      external,
+      cache: true,
+    },
+    {
+      input,
+      output: {
+        dir: 'dist',
+        format: 'commonjs',
+        entryFileNames: '[name]/index.d.ts',
+      },
+      plugins: [dts()],
+    },
+  ])
+}
--- src/__tests__/react/contexts/craete-atoms-context.spec.tsx
+++ src/__tests__/react/contexts/craete-atoms-context.spec.tsx
@@ -1,9 +1,14 @@
-import { render, screen } from '@testing-library/react'
+import { act, render, screen } from '@testing-library/react'
 import { createAtomsContext } from '~/react/index.js'
-import React from 'react'
-import { atom } from 'jotai'
+import { atom, getDefaultStore } from 'jotai'
+import type { FC, PropsWithChildren } from 'react'
+
+import userEvent from '@testing-library/user-event'
+
+import { setStore } from '~/index.js'
 
 describe('createAtomsContext', () => {
+  setStore(getDefaultStore())
   const context = createAtomsContext(
     {
       a: atom(0),
@@ -27,11 +32,12 @@ describe('createAtomsContext', () => {
   // useStoreValue('a')
   //
 
-  const App = () => {
+  const App: FC<PropsWithChildren> = (props) => {
     const aNumber = useStoreValue('a')
     return (
       <Provider>
         <div data-testid="a">{aNumber}</div>
+        {props.children}
       </Provider>
     )
   }
@@ -39,7 +45,33 @@ describe('createAtomsContext', () => {
   it('should render', () => {
     render(<App />)
 
-    expect(screen.getByTestId('a')).toEqual('0')
+    expect(screen.getByTestId('a')).toHaveTextContent('0')
+  })
+
+  it('should increment', async () => {
+    const IncButton = () => {
+      // const [count, setCount] = useState(2)
+      const plusA = useContextActions().increment
+      const inc = () => {
+        // setCount((current) => current + 1)
+        plusA()
+      }
+      return (
+        <button onClick={inc} data-testid="inc">
+          inc
+        </button>
+      )
+    }
+    render(
+      <App>
+        <IncButton />
+      </App>,
+    )
+
+    await userEvent.click(screen.getByTestId('inc'))
+    act(() => {
+      expect(screen.getByTestId('a')).toHaveTextContent('1')
+    })
   })
 })
 // const context = createAtomsContext(
