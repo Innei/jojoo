a1c9b38a5cccb5e247a3e916322d98b1ecac3f17
Innei <i@innei.in>
2023-12-29 18:23:00 +0800

    fix: get singleton jotai store
    Signed-off-by: Innei <i@innei.in>

--- src/lib/init.ts
+++ src/lib/init.ts
@@ -2,13 +2,21 @@ import { getDefaultStore } from 'jotai'
 
 let jotaiStore: ReturnType<typeof getDefaultStore> | undefined
 
+declare const globalThis: any
+
+const JOTAI_GLOBAL_SINGLETON = Symbol()
 export const setGlobalStore = (store: typeof jotaiStore) => {
   jotaiStore = store
+
+  globalThis[JOTAI_GLOBAL_SINGLETON] = store
 }
 
 export const getGlobalStore = () => {
   if (jotaiStore) return jotaiStore
+  if (globalThis[JOTAI_GLOBAL_SINGLETON])
+    return globalThis[JOTAI_GLOBAL_SINGLETON]
   const defaultStore = getDefaultStore()
   jotaiStore = defaultStore
+  globalThis[JOTAI_GLOBAL_SINGLETON] = defaultStore
   return defaultStore
 }
